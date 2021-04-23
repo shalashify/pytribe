ab669d37eaa0b1f8bc9922d9062bd4c3a145a86d
Alec Shalashou <alec.shalashou@softquadrat.de>
2021-04-23 17:27:44 +0200

    Optimized work with sample folders, PEP8, hello screen

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # pytribe
+
 Use Korg Electribe 2 as MIDI Sequencer to trigger samples from computer.
+
+Can be useful to arrange and test different sample sets before loading them to Electribe Sampler (or any other sampler) or use computer as a sample player in combination with Electribe synths.
 
 # Functions
 
@@ -9,7 +12,7 @@ Samples are loaded from source directory (configure in .ini file).
 Source directory can include multiple sets e.g.
 
 <pre>
-.
+Sample_Dir
 ├── 100_Sample_Set_1
 │   ├── 01_Kick.wav
 │   ├── 02_Snare.wav
@@ -29,10 +32,43 @@ Source directory can include multiple sets e.g.
 
 Number of the set (100, 200) can be selected via command prompt or set via .ini file.
 
-Number of the sample (01, 02, 15) within the set corresponds to Electribe 2 pad number (upper row being 1-8, lower row 9-16).
-Number of the sample is thus used to map a sample to a MIDI Channel. Only channels that have samples assigned to them will be treated as relevant. Thus it is possible to trigger samples at computer and play Electribe's own synths in parallel.
+Number of the sample (01, 02, 15) within the set corresponds to Electribe 2 pad number (upper row being 1-8, lower row 9-16). Number of the sample is thus used to map a sample to a MIDI Channel. Only channels that have samples assigned to them will be treated as relevant. Thus it is possible to trigger samples at computer and play Electribe's own synths in parallel.
 
 Level knob controls of the respective channel at Electribe controls the level of each sample.
+
+# Example
+
+```
+amb:pytribe alec$ python3 pytribe.py alec.ini
+
+
+               _____ ___ ___ ___ ___
+      _ __ _  |_   _| _ \_ _| _ ) __|
+     | '_ \ || || | |   /| || _ \ _|
+     | .__/\_, ||_| |_|_\___|___/___|
+     |_|   |__/
+
+
+
+Directory: /Users/alec/Music Production/Tools/Samples/pytribe
+Contains Sets
+  /Users/alec/Music Production/Tools/Samples/pytribe/228_Vinyl_Set_1/
+  /Users/alec/Music Production/Tools/Samples/pytribe/229_Vinyl_Set_2/
+  /Users/alec/Music Production/Tools/Samples/pytribe/230_Standard_Set_1/
+  /Users/alec/Music Production/Tools/Samples/pytribe/300_experiment/
+  /Users/alec/Music Production/Tools/Samples/pytribe/232_Standard_Set_3/
+  /Users/alec/Music Production/Tools/Samples/pytribe/231_Standard_Set_2/
+Which set number to use? (default: 100) 228
+Loading: /Users/alec/Music Production/Tools/Samples/pytribe/228_Vinyl_Set_1
+   MIDI CHANNEL 09 --> 09_Kick.wav
+   MIDI CHANNEL 10 --> 10_Snare.wav
+   MIDI CHANNEL 11 --> 11_Shaker.wav
+   MIDI CHANNEL 12 --> 12_Closed_Hat.wav
+   MIDI CHANNEL 13 --> 13_Open_Hat.wav
+   MIDI CHANNEL 15 --> 15_Tom.wav
+Ready to rock! Start your Sequencer!
+CTRL+C to exit
+```
 
 # Dependencies
 - pygame
--- pytribe.py
+++ pytribe.py
@@ -15,141 +15,158 @@ import configparser
 pygame.init()
 pygame.mixer.init()
 
-do_play = True
-midi_in = mido.open_input()
+_do_play = True
+_midi_in = mido.open_input()
 
-def load_samples(samples_path):
-    global samples
-    global channels
-    global total_channels
+def load_samples(_samples_path):
+    global _samples
+    global _channels
+    global _total_channels
 
-    samples = [None] * (total_channels + 1)
-    channels = [None] * (total_channels + 1)
-    files = [None] * (total_channels + 1)
+    _samples = [None] * (_total_channels + 1)
+    _channels = [None] * (_total_channels + 1)
+    _files = [None] * (_total_channels + 1)
 
-    is_loaded = False
+    _is_loaded = False
 
     # Check if directory exists
-    if(len(glob.glob(samples_path)) == 0):
+    if(len(glob.glob(_samples_path)) == 0):
         return False
 
     # Check wav files
-    all_samples = glob.glob(samples_path + "[0-9][0-9]*.wav")
+    _all_samples = glob.glob(_samples_path + "[0-9][0-9]*.wav")
 
-    for sample in all_samples:
-        path, file = os.path.split(sample)
-        channel = int(file.split("_")[0]) or -1
-        if(channel > 0 and channel <= total_channels and len(sample) > 0):
-           samples[channel] = pygame.mixer.Sound(sample)
-           channels[channel] = pygame.mixer.Channel(channel)
-           files[channel] = file
-           is_loaded = True
+    for _sample in _all_samples:
+        _path, _file = os.path.split(_sample)
+        _channel = int(_file.split("_")[0]) or -1
+        if(_channel > 0 and _channel <= _total_channels and len(_sample) > 0):
+           _samples[_channel] = pygame.mixer.Sound(_sample)
+           _channels[_channel] = pygame.mixer.Channel(_channel)
+           _files[_channel] = _file
+           _is_loaded = True
 
-    print("============================================================================")
-    print('Loading sample set: ' + path)
-    print('Mapped samples:')
-    for i in range(len(samples)-1):
-        if(files[i] != None):
-            print("   MIDI CHANNEL " + str(i).zfill(2) + " --> " + str(files[i]))
-    print("============================================================================")
+    print('Loading: ' + _path)
+    for _i in range(len(_samples)):
+        if(_files[_i] != None):
+            print("   MIDI CHANNEL " + str(_i).zfill(2) + " --> " + str(_files[_i]))
 
-    return is_loaded
+    return _is_loaded
 
-def play():
-    global do_play
-    global samples
-    global cc_level
-    global midi_in
+def _play():
+    global _do_play
+    global _samples
+    global _cc_level
+    global _midi_in
 
-    while do_play and midi_in.iter_pending():
-        msg = midi_in.receive()
-        if msg.type == "note_on" :
+    while _do_play and _midi_in.iter_pending():
+        _msg = _midi_in.receive()
+        if _msg.type == "note_on" :
             #print(msg.channel)
-            if(samples[msg.channel + 1] != None):
-                channels[msg.channel + 1].play(samples[msg.channel + 1])
-        elif msg.type == "control_change":
-            if msg.control == cc_level: # level
-                if(channels[msg.channel + 1] != None):
-                    channels[msg.channel + 1].set_volume(float(msg.value/127))
+            if(_samples[_msg.channel + 1] != None):
+                _channels[_msg.channel + 1].play(_samples[_msg.channel + 1])
+        elif _msg.type == "control_change":
+            if _msg.control == cc_level: # level
+                if(_channels[_msg.channel + 1] != None):
+                    _channels[_msg.channel + 1].set_volume(float(_msg.value/127))
             else:
                 pass # Future logic for other cc
-        elif msg.type == "program_change":
+        elif _msg.type == "program_change":
             #print(msg.program)
             pass # Future program change logic
-        elif(str(msg.type) == "stop"):
+        elif(str(_msg.type) == "stop"):
             #Possible logic to stop
             #do_play = False
             #print(msg.type)
             pass
-        elif(msg.type not in ("note_on", "clock", "note_off")):
+        elif(_msg.type not in ("note_on", "clock", "note_off")):
             #way to monitor remaining messages
             #print(msg.type)
             pass
 
-def load_config(config_file):
-    config = configparser.ConfigParser()
+def _load_config(_config_file):
+    _config = configparser.ConfigParser()
 
-    if(not os.path.isfile(config_file)):
-        print("Config file not found " + config_file)
+    if(not os.path.isfile(_config_file)):
+        print("Config file not found " + _config_file)
         exit()
 
-    config.read(config_file)
+    _config.read(_config_file)
 
-    samples_dir = config['samples'].get('samples_dir', 'Samples')
-    default_samples_set = config['samples'].get('default_set', '100')
-    total_channels = int(config['midi'].get('total_channels', '[16]'))
-    cc_level = int(config['midi'].get('cc_level', '7'))
+    _samples_dir = _config['samples'].get('samples_dir', 'Samples')
+    _default_samples_set = _config['samples'].get('default_set', '100')
+    _total_channels = int(_config['midi'].get('total_channels', '[16]'))
+    _cc_level = int(_config['midi'].get('cc_level', '7'))
 
-    return samples_dir, default_samples_set, total_channels, cc_level
+    return _samples_dir, _default_samples_set, _total_channels, _cc_level
 
-def restart():
-    global do_play
-    restart = input('Load another sample set? (default: Yes) ') or "Y"
-    if(restart in ("Yes", "Y", "Yeah", "y", "yes")):
-        do_play = True
-        main()
+def _restart():
+    global _do_play
+    _restart = input('Load another sample set? (default: Yes) ') or "Y"
+    if(_restart in ("Yes", "Y", "Yeah", "y", "yes")):
+        _do_play = True
+        _main()
     else:
         os._exit(1)
 
-def main():
-    global do_play
-    global total_channels
-    global cc_level
+def _hello():
+    #https://patorjk.com/software/taag/#p=display&f=Small&t=pyTRIBE
+    _hello = """
 
+               _____ ___ ___ ___ ___
+      _ __ _  |_   _| _ \_ _| _ ) __|
+     | '_ \ || || | |   /| || _ \ _|
+     | .__/\_, ||_| |_|_\___|___/___|
+     |_|   |__/
+
+
+    """
+    print(_hello)
+
+def _main():
+    global _do_play
+    global _total_channels
+    global _cc_level
+    global _output_line
     try:
         if(len(sys.argv) > 1):
-            config_file = sys.argv[1] or 'default.ini'
+            _config_file = sys.argv[1] or 'default.ini'
         else:
-            config_file = 'default.ini'
+            _config_file = 'default.ini'
 
-        samples_dir, default_samples_set, total_channels, cc_level = load_config(config_file)
+        _samples_dir, _default_samples_set, _total_channels, _cc_level = _load_config(_config_file)
+        _sample_sets = glob.glob(_samples_dir + "/[0-9]*/")
 
-        print("============================================================================")
-        print("Samples Directory: " + samples_dir)
-        print("Following Sets Found: ")
-        sample_sets = glob.glob(samples_dir + "/*/")
-        for sample_set in sample_sets:
-            print("  " + sample_set)
+        print("Directory: " + _samples_dir)
+        if(len(_sample_sets) > 0) :
+            print("Contains Sets ")
+            for _sample_set in _sample_sets:
+                print("  " + _sample_set)
 
-        current_set = input('Which set number to use? (default: ' + default_samples_set + ') ') or default_samples_set
-        samples_path = samples_dir + "/" + str(current_set) + "_*/"
+            _current_set = input('Which set number to use? (default: ' + _default_samples_set + ') ') or _default_samples_set
+            _samples_path = _samples_dir + "/" + str(_current_set) + "_*/"
 
-        pygame.mixer.set_num_channels(total_channels + 1)
+            pygame.mixer.set_num_channels(_total_channels + 1)
 
-        if(load_samples(samples_path)):
-            do_play = True
-            print('Ready to rock! Start your Sequencer!')
-            print('CTRL+C to exit')
-            try:
-                play()
-            except KeyboardInterrupt:
-                restart()
+            if(load_samples(_samples_path)):
+                _do_play = True
+                print('Ready to rock! Start your Sequencer!')
+                print('CTRL+C to exit')
+                try:
+                    _play()
+                except KeyboardInterrupt:
+                    _restart()
+            else:
+                print("No samples could be loaded from " + _samples_path)
+                _main()
         else:
-            print("No samples could be loaded from: " + samples_path)
-            main()
+            print("No sample sets found!")
+            print("Naming pattern for a set should be /[0-9]*/ (e.g. 100_set)")
+            print("Either correct a path in the ini file or put some sets in")
+            os._exit(1)
     except KeyboardInterrupt:
         pass
 
 
 if __name__ == "__main__":
-    main()
+    _hello()
+    _main()
